6af94c0fa8fb7a19882d58a85e236db157539d28
Kid <44045911+kidonng@users.noreply.github.com>
2021-08-25 13:42:31 +0000

    Fix search box display

--- internal/explore/search_view.go
+++ internal/explore/search_view.go
@@ -4,7 +4,6 @@ import "github.com/rivo/tview"
 
 func BuildSearchView(repo string) *tview.InputField {
 	searchView := tview.NewInputField()
-	searchView.SetBorder(true)
 	searchView.SetLabel(repo)
 	searchView.SetFieldBackgroundColor(0)
 	searchView.SetLabelWidth(len(repo) + 1)
